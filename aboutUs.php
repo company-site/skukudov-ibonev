70ad7113f6fc3b1955021b7ef5c420737031d47b
skukudov <stanimir.kukudov@netea.bg>
2015-01-03 00:10:05 +0200

    skeleton

---
+++ aboutUs.php
@@ -0,0 +1,8 @@
+<?php
+include_partial('_header.php', array('title' => 'about us'));
+?>
+<div class="container">
+    <div class="row">
+        about us
+    </div>
+</div>
--- index.php
+++ index.php
@@ -1,2 +1,27 @@
 <?php
-  echo 'hello world2';
+$page = isset($_GET['page']) ? $_GET['page'] : '';
+// all available pages
+$pages = array('about-us');
+if (!empty($page)) {
+    // page is in the available pages
+    if (in_array($page, $pages)) {
+        // remove dash and uppercase after it
+        $page = lcfirst(implode(array_map(create_function('$v', 'return ucfirst($v);'), explode('-', $page)))) . '.php';
+    }
+    // page exist
+    if (file_exists($page)) {
+        require $page;
+        include_partial('_footer.php');
+    } else {
+        echo 'Page not found. Return to <a href="/">Home</a>';
+    }
+}
+
+function include_partial($fileName, $vars = array())
+{
+    foreach ($vars as $varName => $varValue) {
+        ${$varName} = $varValue;
+    }
+
+    require 'partial/' . $fileName;
+}
