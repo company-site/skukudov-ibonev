27da5b84baf5ee223828060715367a1d65352dc4
skukudov <stanimir.kukudov@netea.bg>
2015-01-03 10:58:27 +0200

    skeleton

--- index.php
+++ index.php
@@ -34,11 +34,13 @@ if (!empty($page)) {
     }
 }
 
+// include partial files
 function include_partial($fileName, $vars = array())
 {
+    // initialize vars
     foreach ($vars as $varName => $varValue) {
         ${$varName} = $varValue;
     }
-
+    // require partial
     require 'partial/' . $fileName;
 }
