26447758a0b48d13d4c415ca4130d3dd6d9432e5
Home <Home@Home-PC>
2015-02-01 21:45:41 +0200

    Style changes

--- app/navigation/navigation.js
+++ app/navigation/navigation.js
@@ -13,31 +13,12 @@
         var vm = this;
         vm.isOnepage = SessionService.getValue('onePage');
         vm.isResponsive = SessionService.getValue('responsiveDesign');
-        console.log(vm.isOnepage);
         
         vm.toggle = function() {
-            var toggle = this;
-
-            e.preventDefault();
-
-            $(toggle).toggleClass('toggle-on')
-                .toggleClass('toggle-off')
-                .addClass('toggle-moving');
-
-            if ($(toggle).hasClass('toggle-on')) {
-                sessionStorage.setItem($(toggle).attr('id'), 'true');
-            } else {
-                 sessionStorage.setItem($(toggle).attr('id'), 'false');
-            }
-
-            setTimeout(function() {
-                $(toggle).removeClass('toggle-moving');
-                window.location.reload();
-            }, 200)
         }
         
         vm.changePage = function(location) {
-            $location.path(location);
+            window.location.href = location;
         }
         
         vm.isActive = function(route) {
