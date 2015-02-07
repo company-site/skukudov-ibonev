b0cfa9922215d2534fc955922e773654154c2005
skukudov <stanimir.kukudov@netea.bg>
2015-02-08 00:42:05 +0200

    styles

--- partial/_header.php
+++ partial/_header.php
@@ -7,7 +7,7 @@
     <!-- JQ -->
     <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <script src="../vendor/js/jquery.easing.min.js"></script>
-    
+
     <!-- Bootstrap -->
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
@@ -15,21 +15,22 @@
     <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">-->
     <!-- Latest compiled and minified JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
-    
+
     <!-- Grayscale Theme css and js -->
     <link rel="stylesheet" href="../vendor/css/grayscale.css">
-<!--    <script src="../vendor/js/grayscale.js"></script>-->
+    <!--    <script src="../vendor/js/grayscale.js"></script>-->
     <!-- Grayscale Custom Fonts -->
     <link href="../vendor/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    <link href="http://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet"
+          type="text/css">
     <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
-    
+
     <!-- custom css and js -->
     <link rel="stylesheet" href="../css/styles.css">
     <script src="../js/dynamicPage.js"></script>
     <link href="/vendor/toastr/toastr.css" rel="stylesheet"/>
     <script src="/vendor/toastr/toastr.js"></script>
-    
+
     <!-- angular -->
     <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.3.8/angular.js"></script>
     <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.3.8/angular-route.js"></script>
@@ -46,16 +47,14 @@
     <script src="../vendor/button.switcher/prefixfree.min.js"></script>
     <!-- button switcher end -->
 
-    <base href="/" />
+    <base href="/"/>
     <title ng-bind="title"><?php // echo $title; ?></title>
     <?php // echo $head; ?>
 </head>
 <body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
-    <div class="conatiner">
-        <div ng-include="'/app/navigation/navigation.html'"></div>
-        <br/><br/>
-            <header class="intro">
-                <div class="intro-body">
-                    <div ng-view=""></div>
-                </div>
-            </header>
+<div class="conatiner">
+    <div ng-include="'/app/navigation/navigation.html'"></div>
+    <br/><br/>
+    <header class="intro">
+        <div ng-view=""></div>
+    </header>
