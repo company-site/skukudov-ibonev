ddbce89e8b26c48fe9d133e93cf1f801c5d27e2a
skukudov <stanimir.kukudov@netea.bg>
2015-02-07 23:05:34 +0200

    resizers

--- partial/_footer.php
+++ partial/_footer.php
@@ -1,36 +1,79 @@
-    </div>
-    <!-- inject:js -->
-    <!-- Bootstrapping -->
-    <script src="../app/app.module.js"></script>
-    <script src="../app/app.config.js"></script>
-    <script src="../app/app.js"></script>
+</div>
+<!-- inject:js -->
+<!-- Bootstrapping -->
+<script src="../app/app.module.js"></script>
+<script src="../app/app.config.js"></script>
+<script src="../app/app.js"></script>
 
-    <!-- core module -->
-    <script src="../app/core/core.module.js"></script>
-    
-    <!-- home -->
-    <script src="../app/home/home.js"></script>
-    <!-- about -->
-    <script src="../app/about/about.js"></script>
-    <!-- demonstrations -->
-    <script src="../app/demonstrations/demonstrations.js"></script>
-    <!-- navigation -->
-    <script src="../app/navigation/navigation.js"></script>
-    <!-- dynamic page -->
-    <script src="../app/dynamic-page/dynamic-page.js"></script>
-    <!-- contact page -->
-    <script src="../app/contact/contact.js"></script>
-    
-    <!-- factories -->
-    <script src="../app/core/factories/session.factory.js"></script>
-    
-    <!-- Footer -->
-    <footer>
-        <div class="container text-center">
-            <p>Copyright &copy; ISComp 2014</p>
+<!-- core module -->
+<script src="../app/core/core.module.js"></script>
+
+<!-- home -->
+<script src="../app/home/home.js"></script>
+<!-- about -->
+<script src="../app/about/about.js"></script>
+<!-- demonstrations -->
+<script src="../app/demonstrations/demonstrations.js"></script>
+<!-- navigation -->
+<script src="../app/navigation/navigation.js"></script>
+<!-- dynamic page -->
+<script src="../app/dynamic-page/dynamic-page.js"></script>
+<!-- contact page -->
+<script src="../app/contact/contact.js"></script>
+
+<!-- factories -->
+<script src="../app/core/factories/session.factory.js"></script>
+
+<!-- Footer -->
+<footer>
+    <div class="container">
+        <div class="text-center">
+            <p>Check how the website looks on different devices</p>
         </div>
-    </footer>
-
-
+        <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+            Phones:
+            <ul>
+                <li><a href="#" onclick="window.open(this, '_blank', 'height=480, width=320')"><span
+                            class="glyphicon glyphicon-phone" aria-hidden="true"></span> 320x480(Portrait)</a></li>
+                <li><a href="#" onclick="window.open(this, '_blank', 'height=320, width=480')"><span
+                            class="glyphicon glyphicon-phone" aria-hidden="true"></span> 480x320 (Landscape)</a></li>
+                <li><a href="#" onclick="window.open(this, '_blank', 'height=568, width=320')"><span
+                            class="glyphicon glyphicon-phone" aria-hidden="true"></span> 320x568 (Portrait)</a></li>
+                <li><a href="#" onclick="window.open(this, '_blank', 'height=320, width=568')"><span
+                            class="glyphicon glyphicon-phone" aria-hidden="true"></span> 568x320 (Landscape)</a></li>
+            </ul>
+        </div>
+        <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+            Tablets :
+            <ul>
+                <li><a href="#" onclick="window.open(this, '_blank', 'height=800, width=600')"><span
+                            class="glyphicon fa fa-tablet" aria-hidden="true"></span> 600x800 (Portrait)</a></li>
+                <li><a href="#" onclick="window.open(this, '_blank', 'height=600, width=800')"><span
+                            class="glyphicon fa fa-tablet" aria-hidden="true"></span> 800x600 (Landscape)</a></li>
+                <li><a href="#" onclick="window.open(this, '_blank', 'height=1024, width=768')"><span
+                            class="glyphicon fa fa-tablet" aria-hidden="true"></span> 768x1024 (Portrait)</a></li>
+                <li><a href="#" onclick="window.open(this, '_blank', 'height=768, width=1024')"><span
+                            class="glyphicon fa fa-tablet" aria-hidden="true"></span> 1024x768 (Landscape)</a></li>
+            </ul>
+        </div>
+        <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+            Monitors :
+            <ul>
+                <li><a href="#" onclick="window.open(this, '_blank', 'height=1280, width=800')"><span
+                            class="glyphicon fa fa-desktop" aria-hidden="true"></span> 800x1280 (Portrait)</a></li>
+                <li><a href="#" onclick="window.open(this, '_blank', 'height=800, width=1280')"><span
+                            class="glyphicon fa fa-desktop" aria-hidden="true"></span> 1280x800 (Landscape)</a></li>
+                <li><a href="#" onclick="window.open(this, '_blank', 'height=1366, width=768')"><span
+                            class="glyphicon fa fa-desktop" aria-hidden="true"></span> 768x1366 (Portrait)</a></li>
+                <li><a href="#" onclick="window.open(this, '_blank', 'height=768, width=1366')"><span
+                            class="glyphicon fa fa-desktop" aria-hidden="true"></span> 1366x768 (Landscape)</a></li>
+                <li><a href="#" onclick="window.open(this, '_blank', 'height=2560, width=1600')"><span
+                            class="glyphicon fa fa-desktop" aria-hidden="true"></span> 1600x2560 (Portrait)</a></li>
+                <li><a href="#" onclick="window.open(this, '_blank', 'height=1600, width=2560')"><span
+                            class="glyphicon fa fa-desktop" aria-hidden="true"></span> 2560x1600 (Landscape)</a></li>
+            </ul>
+        </div>
+    </div>
+</footer>
 </body>
 </html>
