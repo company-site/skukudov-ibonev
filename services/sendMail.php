724a82cfe1f06f691459721454d0ba1395d3fe2a
Home <Home@Home-PC>
2015-03-03 23:18:47 +0200

    Send mail merge

--- services/sendMail.php
+++ services/sendMail.php
@@ -1,21 +1,32 @@
 <?php
+require '../vendor/PHPMailer/PHPMailerAutoload.php';
+
 $response = '';
 $contactForm = array();
 
-if(isset($_REQUEST['contactForm'])) {
-    $contactForm  = (array)json_decode($_REQUEST['contactForm']);
+if (isset($_REQUEST['contactForm'])) {
+    $contactForm = (array)json_decode($_REQUEST['contactForm']);
 
-    $message = $contactForm['message'] 
-            . PHP_EOL . "Email " . $contactForm['email']
-            . PHP_EOL . "Name: " . $contactForm['name'];
-    
-    if(mail('info@websail.net', 'Contact form mesage', $message)) {
-        $response['message'] = "Mail sent successfully!";
+    $message = $contactForm['message']
+        . PHP_EOL . "Email " . $contactForm['email']
+        . PHP_EOL . "Name: " . $contactForm['name'];
+
+    $mail = new PHPMailer();
+
+    $mail->From = $contactForm['email'];
+    $mail->FromName = $contactForm['name'];
+    $mail->addAddress('info@websail.net');
+    $mail->Subject = 'Contact form message';
+    $mail->Body = $contactForm['message'];
+
+    if ($mail->send()) {
+        $response['message'] = "Mail sent!";
         http_response_code(200);
     } else {
+        error_log($mail->ErrorInfo);
         $response['message'] = 'Mail sending failed. Please try again!';
         http_response_code(500);
-    }    
+    }
 } else {
     $response['message'] = "No data sent!";
     http_response_code(404);
