908a10690aed43241d565d57547d273fd93a682b
skukudov <stanimir.kukudov@netea.bg>
2015-03-10 12:03:53 +0200

    services changes

--- app/services/services.js
+++ app/services/services.js
@@ -5,12 +5,20 @@
         .module('app')
         .controller('Services', Services);
 
-    Services.$inject = [];
+    Services.$inject = ['$sce','$routeParams', '$rootScope'];
 
-    function Services() {
+    function Services($sce, $routeParams, $rootScope) {
 
         /*jshint validthis: true */
         var vm = this;
+        $rootScope.noFooter = 'false';
+        $rootScope.homeLink = '/';
+
+        vm.iframeSource = $sce.trustAsResourceUrl("/?nofooter=true");
+        if (typeof $routeParams.nofooter != "undefined") {
+            $rootScope.noFooter = $routeParams.nofooter;
+            $rootScope.homeLink = "/?nofooter=true";
+        }
 
         vm.welcomeText = 'Services page';
     }
