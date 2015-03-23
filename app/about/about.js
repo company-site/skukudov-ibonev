e036df43d64fd3c06655c555f72ce7a7486ae65d
skukudov <stanimir.kukudov@netea.bg>
2015-03-23 12:40:04 +0200

    contact us - google +

--- app/about/about.js
+++ app/about/about.js
@@ -15,11 +15,11 @@
         vm.welcomeText = 'About us';
         
         vm.team = [
-            {name: 'Mihail Mihaylov', title: 'CEO / Chief Executive Officer', picture: 'mihail.png', description: 'Description', contacts: {facebook: 'https://www.facebook.com/mihail.mihailov.ol', linkedin: 'https://www.linkedin.com/in/mmihaylov', twitter: '', email: 'm.mihaylov@softologic.com'}},
-            {name: 'Stanimir Kukudov', title: 'CTO / Chief Technical Officer', picture: 'stanimir.png', description: 'Description', contacts: {facebook: 'https://www.facebook.com/stanimir.kukudov', linkedin: 'https://bg.linkedin.com/in/stkukudov', twitter: '', email: 'st.kukudov@softologic.com'}},
-            {name: 'Iliyan Bonev', title: 'CIO / Chief Information Officer', picture: 'ilian.png', description: 'Description', contacts: {facebook: 'https://www.facebook.com/iliyan.bonev.3', linkedin: 'https://bg.linkedin.com/in/boneff', twitter: '', email: 'i.bonev@softologic.dk'}},
-            {name: 'Vanya Zdravkova', title: 'COO / Chief Operations Officer', picture: 'vania.png', description: 'Description', contacts: {facebook: 'fb', linkedin: 'linkedin', twitter: 'twitter', email: 'email'}},
-            {name: 'Svetoslav Kukudov', title: 'Lead Designer', picture: 'svetoslav.png', description: 'Description', contacts: {facebook: 'https://www.facebook.com/SvetoslavKukudov', linkedin: 'https://bg.linkedin.com/in/svkukudov', twitter: '', email: 'sv.kukudov@softologic.com'}}
+            {name: 'Mihail Mihaylov', title: 'CEO / Chief Executive Officer', picture: 'mihail.png', description: 'Description', contacts: {facebook: 'https://www.facebook.com/mihail.mihailov.ol', linkedin: 'https://www.linkedin.com/in/mmihaylov', twitter: '', google: '', email: 'm.mihaylov@softologic.com'}},
+            {name: 'Stanimir Kukudov', title: 'CTO / Chief Technical Officer', picture: 'stanimir.png', description: 'Description', contacts: {facebook: 'https://www.facebook.com/stanimir.kukudov', linkedin: 'https://bg.linkedin.com/in/stkukudov', twitter: '', google: 'https://plus.google.com/u/0/112647689638721812370', email: 'st.kukudov@softologic.com'}},
+            {name: 'Iliyan Bonev', title: 'CIO / Chief Information Officer', picture: 'ilian.png', description: 'Description', contacts: {facebook: 'https://www.facebook.com/iliyan.bonev.3', linkedin: 'https://bg.linkedin.com/in/boneff', twitter: '', google: '', email: 'i.bonev@softologic.dk'}},
+            {name: 'Vanya Zdravkova', title: 'COO / Chief Operations Officer', picture: 'vania.png', description: 'Description', contacts: {facebook: 'https://www.facebook.com/vanya.zdravkova', linkedin: 'https://www.linkedin.com/in/vanyazdravkova', twitter: '', google: 'https://plus.google.com/u/0/106260867961197044827', email: 'v.zdravkova@softologic.dk'}},
+            {name: 'Svetoslav Kukudov', title: 'Lead Designer', picture: 'svetoslav.png', description: 'Description', contacts: {facebook: 'https://www.facebook.com/SvetoslavKukudov', linkedin: 'https://bg.linkedin.com/in/svkukudov', twitter: '', google: '', email: 'sv.kukudov@softologic.com'}}
         ];
     }
 })();
