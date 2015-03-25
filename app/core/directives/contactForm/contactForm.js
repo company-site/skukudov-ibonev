776536e6e99cb6ef22b24f7d7965f9611b75905e
Home <Home@Home-PC>
2015-03-25 23:52:32 +0200

    Contact form validation on IOS fix

--- app/core/directives/contactForm/contactForm.js
+++ app/core/directives/contactForm/contactForm.js
@@ -4,24 +4,35 @@
             return {
                 restrict: 'E',
                 controller: function($scope, $rootScope, $http){
-                    $scope.contactForm = {};
-        
-                    $scope.submitForm = function () {
-                        $http({
-                            url: '/mail/sendMail.php',
-                            method: 'POST',
-                            params: { contactForm: JSON.stringify($scope.contactForm) }
-                        })
-                        .success(function(data, status) {
-                            console.log(data, status);
-                            toastr.success(data.message);
-                            $scope.contactForm = {};
-                            $rootScope.$broadcast('contactFormSuccess', "sent");
-                        })
-                        .error(function(data, status) {
-                            console.log(data, status);
-                            toastr.error(data.message); 
-                        });
+                    $scope.form = {};
+                    var fields = ['name', 'email', 'website', 'company', 'message'];
+                    
+                    $scope.submitForm = function (form) {
+                        console.log(form.$vaild, form);
+                        if(form.$vaild) {
+                            console.log('valid');
+                            $http({
+                                url: '/mail/sendMail.php',
+                                method: 'POST',
+                                params: { contactForm: JSON.stringify($scope.form) }
+                            })
+                            .success(function(data, status) {
+                                console.log(data, status);
+                                toastr.success(data.message);
+                                $scope.contactForm = {};
+                                $rootScope.$broadcast('contactFormSuccess', "sent");
+                            })
+                            .error(function(data, status) {
+                                console.log(data, status);
+                                toastr.error(data.message); 
+                            });
+                        } else {
+                            angular.forEach(fields, function(field) {
+                                console.log(form[field]);
+                                form[field].$dirty = true;
+                            });
+                        }
+                        
                     };
                 },
                 templateUrl: '/app/core/directives/contactForm/contactForm.html'
