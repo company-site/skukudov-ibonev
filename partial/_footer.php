62972cffe9d2cdbc6f04d037e584226061796db1
skukudov <stanimir.kukudov@netea.bg>
2015-06-04 21:21:54 +0300

    Footer changes

--- partial/_footer.php
+++ partial/_footer.php
@@ -1,51 +1,67 @@
-    </div>
-    </div>
-    <!-- inject:js -->
-    <!-- Bootstrapping -->
-    <script src="../app/app.module.js"></script>
-    <script src="../app/app.config.js"></script>
-    <script src="../app/app.js"></script>
+</div>
+</div>
+<!-- inject:js -->
+<!-- Bootstrapping -->
+<script src="../app/app.module.js"></script>
+<script src="../app/app.config.js"></script>
+<script src="../app/app.js"></script>
 
-    <!-- core module -->
-    <script src="../app/core/core.module.js"></script>
+<!-- core module -->
+<script src="../app/core/core.module.js"></script>
 
-    <!-- home -->
-    <script src="../app/home/home.js"></script>
-    <!-- about -->
-    <script src="../app/about/about.js"></script>
-    <!-- demonstrations -->
-    <script src="../app/demonstrations/demonstrations.js"></script>
-    <!-- technologies -->
-    <script src="../app/technologies/technologies.js"></script>
-    <!-- services -->
-    <script src="../app/services/services.js"></script>
-    <!-- navigation -->
-    <script src="../app/navigation/navigation.js"></script>
-    <!-- dynamic page -->
-    <script src="../app/dynamic-page/dynamic-page.js"></script>
-    <!-- contact page -->
-    <script src="../app/contact/contact.js"></script>
+<!-- home -->
+<script src="../app/home/home.js"></script>
+<!-- about -->
+<script src="../app/about/about.js"></script>
+<!-- demonstrations -->
+<script src="../app/demonstrations/demonstrations.js"></script>
+<!-- technologies -->
+<script src="../app/technologies/technologies.js"></script>
+<!-- services -->
+<script src="../app/services/services.js"></script>
+<!-- navigation -->
+<script src="../app/navigation/navigation.js"></script>
+<!-- dynamic page -->
+<script src="../app/dynamic-page/dynamic-page.js"></script>
+<!-- contact page -->
+<script src="../app/contact/contact.js"></script>
 
-    <!-- factories -->
-    <script src="../app/core/factories/session.factory.js"></script>
+<!-- factories -->
+<script src="../app/core/factories/session.factory.js"></script>
 
-    <!-- directives -->
-    <script src="../app/core/directives.js"></script>
-    <script src="../app/core/directives/dragabble.js"></script>
-    <script src="../app/core/directives/contactForm/contactForm.js"></script>
+<!-- directives -->
+<script src="../app/core/directives.js"></script>
+<script src="../app/core/directives/dragabble.js"></script>
+<script src="../app/core/directives/contactForm/contactForm.js"></script>
 
-    <!-- vendor -->
-    <!-- angular bind once -->
-    <script src="../vendor/js/once.js"></script>
-    <script src="../vendor/js/lodash.js"></script>
-    <script src="../vendor/js/angular-google-maps.min.js"></script>
-    <script src="../vendor/js/ui-bootstrap-tpls-0.12.1.min.js"></script>
-    
+<!-- vendor -->
+<!-- angular bind once -->
+<script src="../vendor/js/once.js"></script>
+<script src="../vendor/js/lodash.js"></script>
+<script src="../vendor/js/angular-google-maps.min.js"></script>
+<script src="../vendor/js/ui-bootstrap-tpls-0.12.1.min.js"></script>
+
 <!-- Footer -->
-<footer class="container footer-container">
-    <a href="https://www.facebook.com/softologic.dk" class="pull-right" target="_blank"><i class="fa fa-2x fa-facebook-square"></i></a>
-    <a href="#" class="pull-right" target="_blank"><i class="fa fa-2x fa-linkedin-square"></i></a>
-    <a href="#" class="pull-right" target="_blank"><i class="fa fa-2x fa-google-plus-square"></i></a>
-</footer>
+<div class="container-fluid footer-container">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 footer-top">
+                <a href="https://www.facebook.com/softologic.dk" class="pull-right" target="_blank"><i
+                        class="fa fa-2x fa-facebook-square"></i></a>
+                <a href="#" class="pull-right" target="_blank"><i class="fa fa-2x fa-linkedin-square"></i></a>
+                <a href="#" class="pull-right" target="_blank"><i class="fa fa-2x fa-google-plus-square"></i></a>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="footer-bottom-bg">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <p>© 2015 SoftOlogic. All rights reserved. </p>
+            </div>
+        </div>
+    </div>
+</div>
 </body>
 </html>
